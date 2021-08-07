3178301e3724b28dc64865ac4e87e024045253d2
aydenapollo <mcdonaldshane33@gmail.com>
2021-08-07 10:17:10 -0700

    routers set up

---
+++ src/add-item.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class AddItem extends (React.Component) {
+    render() {
+        return (
+            <div>
+                <AddItem/>
+            </div>
+        )
+    }
+}
+
+
+export default AddItem
